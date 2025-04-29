c14edf9ce68f40506ea68f91cd5e21bf08e9e4df
Rharael <60363332+rharael@users.noreply.github.com>
2025-04-29 00:08:14 +0000

    update info

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const TELEGRAM_BOT_TOKEN = "BOT-TOKEN"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "CHAT-TOKEN"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_BOT_TOKEN = "7724100337:AAGQEaxgWwVx4h29D1GoBUjQBE0dCGK_MIQ"; // Substitua pelo token do seu bot
+const TELEGRAM_CHAT_ID = "7724100337"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
